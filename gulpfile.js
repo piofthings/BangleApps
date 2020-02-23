bcad75858be7d6c571e483987208c16f031f7982
Sumit Kumar Maitra <sumitkm@gmail.com>
2020-02-23 00:53:24 +0000

    bunping versions

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,48 @@
-var { fs }  = require("fs");
+var fs = require("fs");
 var { src, dest, parallel, series } = require('gulp');
 var terser = require("gulp-terser");
 
 function minify() {
-  return src('apps/fitclck/dev/fit-clock.js', { base: 'apps/fitclck/dev'})
-        .pipe(terser())
-        .pipe(dest('apps/fitclck'));
-}
-function assets(){
-    return src(['apps/fitclk/dev/**/*.json','apps/fitclck/dev/**/*.png'], { base: 'apps/fitclck/dev/fit-clock'})
-        .pipe(dest("apps/fitclck"));
-}
-function revision(){
-  var settings = require("apps.json");
-  for(var i=0;i<settings.length;i++){
-    if(settings[i].id == "fitclk"){
-      var version = settings[i].version.split('.');
-      var verMajor = version[0];
-      var verMinor = parseInt(version[1]);
-      verMinor ++;
-      settings[i].version = verMajor + '.' + verMinor;
-      fs.writeFile("apps.json",JSON.stringify(settings, null, ' '));
+  return src('apps/fitclck/dev/fit-clock.js', { base: 'apps/fitclck/dev' })
+    .pipe(terser())
+    .pipe(dest('apps/fitclck'));
+};
+
+function assets() {
+  return src(['apps/fitclk/dev/**/*.json', 'apps/fitclck/dev/**/*.png'], { base: 'apps/fitclck/dev/fit-clock' })
+    .pipe(dest("apps/fitclck"));
+};
+
+function revision(done) {
+  fs.readFile('apps.json', 'utf8',function readFileCallback(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      var settings = JSON.parse(data);
+      for (var i = 0; i < settings.length; i++) {
+        if (settings[i].id == "fitclck") {
+          var version = settings[i].version.split('.');
+          var verMajor = version[0];
+          var verMinor = parseInt(version[1]);
+          verMinor++;
+          settings[i].version = verMajor + '.' + verMinor;
+          console.log("version: " + settings[i].version);
+          fs.writeFile("apps.json", JSON.stringify(settings, null, ' '), 'utf8', function(error){
+            if(error){
+              done(error);
+              console.err(error);
+            }
+            else{              
+              done();
+            }
+          })          
+        }        
+      }
     }
-  }
-}
+  });
+};
+
 exports.minify = minify;
 exports.assets = assets;
-exports.version = version;
-exports.default = series(parallel(minify, assets),version);
+exports.revision = revision;
+exports.default = parallel(minify, assets, revision);
